b689233b85fb271029a68987d34fc90ceac75443
rachit02581@mail.com <rachit02581@gmail.com>
2023-11-22 20:51:56 +0530

    final

--- src/App.js
+++ src/App.js
@@ -16,29 +16,66 @@ import TestimonialSection2 from './MyComponents/TestimonialSection2';
 import FaqSection from './MyComponents/FaqSection';
 import LimitedSection from './MyComponents/LimitedSection';
 import { motion, useScroll } from "framer-motion";
+import Lottie from 'react-lottie';
+import animationData from './Lottie-Lego.json';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+
 
 function App() {
-  const { scrollYProgress } = useScroll();
+  // const { scrollYProgress } = useScroll();
+  // const lottieOptions = {
+  //   loop: true, // Set to true if you want the animation to loop
+  //   autoplay: true, // Set to true if you want the animation to start playing as soon as it's loaded
+  //   animationData: animationData, // The animation data
+  //   rendererSettings: {
+  //     preserveAspectRatio: 'xMidYMid slice',
+  //   },
+  // };
   return (
     <div className="App">
-      {console.log(scrollYProgress)}
+      {/* {console.log(scrollYProgress)}
       <motion.div
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
-      />
+      /> */}
       
       
       <Header />
+      
+      <ScrollAnimation animateIn="bounceInRight" animateOut='fadeOut' >
       <SpeakSection />
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="bounceInLeft"  >
       <PotentialSection />
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="bounceInRight" animateOut='fadeOut' >
       <BeginSection/>
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="bounceInLeft" animateOut='fadeOut' >
       <LearnSection/>
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="bounceInRight" animateOut='fadeOut' >
       <WhoSection/>
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="bounceInLeft" animateOut='fadeOut' >
       <MeetSection/>
+      </ScrollAnimation>
+      
+      
       <FeaturedSection/>
       <BonusSection/>
-      <TestimonialSection/>
+      <ScrollAnimation animateIn="bounceInRight" animateOut='fadeOut' >
+        
+        <TestimonialSection/>
       <TestimonialSection2/>
+        </ScrollAnimation>
+      
       <FaqSection/>
       <LimitedSection/>
       <Footer/>
